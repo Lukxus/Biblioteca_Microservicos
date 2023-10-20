e49a69aa359241aaa2f7bdc09e7fce68dc4c1205
Luiz Octavio Tassinari Saraiva <you@example.com>
2023-10-20 16:17:21 -0300

    utilizando controladores

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbconnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -13,21 +13,7 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-})
+routes(app);
 
 app.post("/livros", (req, res) => {
   livros.push(req.body);
---
+++ src/controllers/livroController.js
@@ -0,0 +1,25 @@
+import livro from "../models/Livro.js";
+
+class LivroController {
+    static async listarlivros(req, res) {
+        const listaLivros = await livro.find({});
+        res.status(200).json(listaLivros);
+    }
+
+    static async inserirLivro(req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({
+                message: "livro criado com sucesso",
+                livro: novoLivro
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: "${erro.message} - falha ao cadastrar livro"
+            })
+        }
+
+    }
+}
+
+export default LivroController;
