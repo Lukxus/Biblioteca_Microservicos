ea654d2c7a03fa7627d50c0c977b2f998622443a
Luiz Saraiva <83554105+Lukxus@users.noreply.github.com>
2023-10-21 11:18:55 -0300

    Update dbconnect.js

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-  console.log(process.env.DB_CONNECTION_STRING)
+  console.log(process.env.DB_CONNECTION_STRING);
   mongoose.connect(process.env.DB_CONNECTION_STRING);
   return mongoose.connection;
 };
